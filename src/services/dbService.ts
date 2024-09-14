b18bd469e1ab010ea5ab3f217954c25001a00fe4
Luis Herrera <luis.herrera506@gmail.com>
2024-09-13 23:50:13 -0600

    Moving the files to the ui folder

--- src/services/dbService.ts
+++
@@ -1,8 +0,0 @@
-export const saveWeekData = async (weekData: any) => {
-    console.log("Guardando datos de la semana en la base de datos", weekData);
-  };
-  
-  export const loadWeekData = async (week: Date) => {
-    console.log("Cargando datos de la semana", week);
-  };
-  
